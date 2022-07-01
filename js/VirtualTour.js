b12b054d249b0b0e2d4ade2e77bf54b94fc8b0ea
arsyadn <arsyadnurzaky@gmail.com>
2022-07-01 21:34:41 +0700

    change position at infospot

--- js/VirtualTour.js
+++ js/VirtualTour.js
@@ -2,12 +2,12 @@ let panoHTML = document.querySelector(".pano-lens");
 let sourceImage = 'test.jpg';
 
 // let panorama = new PANOLENS.ImagePanorama('https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg');
-let panorama = new PANOLENS.ImagePanorama('./assets/images/img1_gate.jpg');
 // let panorama2 = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
+let panorama = new PANOLENS.ImagePanorama('./assets/images/img1_gate.jpg');
 let panorama2 = new PANOLENS.ImagePanorama('./assets/images/img2_tugu.jpg');
 
 let infospot = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
-infospot.position.set( 500, 500, 5000 );
+infospot.position.set( -8000, 500, 5000 );
 infospot.addHoverText( "Tugu UG Techno Park" );
 infospot.addEventListener( 'click', function(){
   viewer.setPanorama( panorama2 );
