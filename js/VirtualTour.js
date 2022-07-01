7fb0c225bcf107eaab8f660d1b62bece52dc190b
arsyadn <arsyadnurzaky@gmail.com>
2022-07-01 21:39:49 +0700

    change position in infospot2Tugu

--- js/VirtualTour.js
+++ js/VirtualTour.js
@@ -1,27 +1,27 @@
 let panoHTML = document.querySelector(".pano-lens");
 let sourceImage = 'test.jpg';
 
-// let panorama = new PANOLENS.ImagePanorama('https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg');
-// let panorama2 = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
-let panorama = new PANOLENS.ImagePanorama('./assets/images/img1_gate.jpg');
-let panorama2 = new PANOLENS.ImagePanorama('./assets/images/img2_tugu.jpg');
+// let panorama1Gate = new PANOLENS.ImagePanorama('https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg');
+// let panorama2Tugu = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
+let panoramaGate = new PANOLENS.ImagePanorama('./assets/images/img1_gate.jpg');
+let panoramaTugu = new PANOLENS.ImagePanorama('./assets/images/img2_tugu.jpg');
 
 let infospot = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
 infospot.position.set( -8000, 500, 5000 );
 infospot.addHoverText( "Tugu UG Techno Park" );
 infospot.addEventListener( 'click', function(){
-  viewer.setPanorama( panorama2 );
+  viewer.setPanorama( panorama2Tugu );
 } );
 
 // Widget 2
 let infospot2 = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
-infospot2.position.set( 100, 500, 5000 );
+infospot2.position.set( 10000, 500, 5000 );
 infospot2.addHoverText( "Back to Gate" );
 infospot2.addEventListener( 'click', function(){
-  viewer.setPanorama( panorama );
+  viewer.setPanorama( panorama1Gate );
 } );
-panorama.add( infospot );
-panorama2.add( infospot2 );
+panorama1Gate.add( infospot );
+panorama2Tugu.add( infospot2 );
 
 
 
