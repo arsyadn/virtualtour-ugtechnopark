40d33a4c8b49f91825003cf9ec32b23ac8b854d4
arsyadn <arsyadnurzaky@gmail.com>
2022-07-01 21:41:41 +0700

    fix variable error in virtualtour.js

--- js/VirtualTour.js
+++ js/VirtualTour.js
@@ -1,33 +1,34 @@
 let panoHTML = document.querySelector(".pano-lens");
 let sourceImage = 'test.jpg';
 
-// let panorama1Gate = new PANOLENS.ImagePanorama('https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg');
-// let panorama2Tugu = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
+// let panoramaGate = new PANOLENS.ImagePanorama('https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg');
+// let panoramaTugu = new PANOLENS.ImagePanorama( 'https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/sunset.jpg' );
 let panoramaGate = new PANOLENS.ImagePanorama('./assets/images/img1_gate.jpg');
 let panoramaTugu = new PANOLENS.ImagePanorama('./assets/images/img2_tugu.jpg');
 
-let infospot = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
-infospot.position.set( -8000, 500, 5000 );
-infospot.addHoverText( "Tugu UG Techno Park" );
-infospot.addEventListener( 'click', function(){
-  viewer.setPanorama( panorama2Tugu );
+// Gate
+let infospotGate = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
+infospotGate.position.set( -8000, 500, 5000 );
+infospotGate.addHoverText( "Tugu UG Techno Park" );
+infospotGate.addEventListener( 'click', function(){
+  viewer.setPanorama( panoramaTugu );
 } );
 
-// Widget 2
-let infospot2 = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
-infospot2.position.set( 10000, 500, 5000 );
-infospot2.addHoverText( "Back to Gate" );
-infospot2.addEventListener( 'click', function(){
-  viewer.setPanorama( panorama1Gate );
+// Tugu
+let infospotTugu = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
+infospotTugu.position.set( 10000, 500, 5000 );
+infospotTugu.addHoverText( "Back to Gate" );
+infospotTugu.addEventListener( 'click', function(){
+  viewer.setPanorama( panoramaGate );
 } );
-panorama1Gate.add( infospot );
-panorama2Tugu.add( infospot2 );
+panoramaGate.add( infospotGate );
+panoramaTugu.add( infospotTugu );
 
 
 
 let viewer = new PANOLENS.Viewer({ 
   container: panoHTML
  });
- viewer.add(panorama, panorama2);
+ viewer.add(panoramaGate, panoramaTugu);
  viewer.addUpdateCallback(function(){
 }); 
