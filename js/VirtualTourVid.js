df83643a3e0dd5b8ce1272e6e150fda33bdc6870
arsyadn <arsyadnurzaky@gmail.com>
2022-08-15 12:02:12 +0700

    fix: change ui in index.html

--- js/VirtualTourVid.js
+++
@@ -1,9 +0,0 @@
-var panorama, viewer;
-const panoHTML = document.querySelector(".pano-lens");
-
-panorama = new PANOLENS.VideoPanorama( 'technopark.mp4', { autoplay: true } );
-
-viewer = new PANOLENS.Viewer({
-  container: panoHTML
-});
-viewer.add( panorama );
